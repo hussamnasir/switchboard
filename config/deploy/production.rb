1ff58d4b50503a387eb6929a5c22126ae0660ad9
root <root@switchboard.netlab.uky.edu>
2015-12-04 16:10:51 -0500

    More

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,13 +4,3 @@ set :rails_env, 'production'
 set :deploy_via, :export
 
 set :vhost, 'switchboard.netlab.uky.edu'
-
-# Deploy from local working copy
-#set :repository, "."
-#set :scm, :none
-#set :deploy_via, :copy
-set :copy_exclude, [".git/*", 'config/database.yml', 'config/switchboard.yml']
-
-#set :keep_releases, 25
-
-
